f823504543664f765beb3fa00a13240a84a9afb6
Kim Sandberg <s@ndberg.dk>
2021-02-25 23:40:15 -0800

    sqliteDB

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,39 +1,52 @@
 const { ApolloServer } = require('apollo-server');
+const { PrismaClient } = require(`@prisma/client`)
 const fs = require(`fs`);
 const path = require(`path`);
 
-let links =[{
-    id: `link-0`,
-    url: `www.howtographql.com`,
-    description: `Fullstack tutorial for GraphQL`,
-}]
-
-// 1
-let idCount = links.length
 const resolvers = {
     Query: {
-        info: ()=> `This is the API for Hackernews Clone`,
-        feed: () => links,
+        info: (parent, args, context)=> `The server is up`,
+        feed: async (parent, args, context) => {
+            return context.prisma.link.findMany()
+        },
     },
+
+    
     Mutation: {
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url,
-            }
-            links.push(link)
-            return link
-        }
+        post: (parent, args, context, info) => {
+            const newLink = context.prisma.link.create({
+                data:{
+                    url: args.url,
+                    description: args.description,
+                }
+            })
+            return newLink
+        },          
     },
-}
+};
 
 
-//3 
+
+const prisma = new PrismaClient()
+const {getUserId} = require(`./utils`);
+
 const server = new ApolloServer({
-    typeDefs: fs.readFileSync(path.join(__dirname, `schema.graphql`),`utf8`),
-    resolvers,
-})
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, `schema.graphql`),
+        `utf8`
+        ),
+        resolvers, 
+        context: {( req }) => {  
+        return {
+            ...req,
+            prisma,
+            userId:
+                req && req.headers.authorization
+                ? getUserId(req)
+                :null
+        };
+    }
+});
 
 server
     .listen()
